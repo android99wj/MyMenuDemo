aedd419fb714c7379a1297f518d840780b5f0abb
wj <wj1164664118@163.com>
2018-06-06 15:59:20 +0800

    AndroidResideMenu

---
+++ app/src/main/java/com/wj/menu/AndroidResideMenuActivity.java
@@ -0,0 +1,98 @@
+package com.wj.menu;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import com.special.ResideMenu.ResideMenu;
+import com.special.ResideMenu.ResideMenuItem;
+
+/**
+ * Created by Hannah on 2018/6/6.
+ */
+
+public class AndroidResideMenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+  @InjectView(R.id.main_titlebar_left_tv)
+  TextView mainTitlebarLeftTv;
+
+  private Context    context;
+  private ResideMenu menu;
+
+  @Override
+  protected void onCreate(
+      @Nullable
+          Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_arm);
+    ButterKnife.inject(this);
+    context = this;
+    initMenu();
+  }
+
+  private void initMenu() {
+    //创建侧滑菜单
+    menu = new ResideMenu(context);
+    menu.setBackground(R.color.colorPrimary);
+    menu.attachToActivity(this);
+    menu.setScaleValue(0.7f);
+
+    String[] titles = { "Home", "Records", "Settings" };
+    int[] icons = { R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher };
+    //添加条目
+    for (int i = 0; i < titles.length; i++) {
+      ResideMenuItem item = new ResideMenuItem(context, icons[i], titles[i]);
+      item.setTag(i);
+      item.setOnClickListener(this);
+      menu.addMenuItem(item);
+    }
+
+    //设置菜单的点击
+    menu.setMenuListener(new ResideMenu.OnMenuListener() {
+      @Override
+      public void openMenu() {
+
+      }
+
+      @Override
+      public void closeMenu() {
+
+      }
+    });
+    mainTitlebarLeftTv.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        menu.openMenu(ResideMenu.DIRECTION_LEFT);
+      }
+    });
+  }
+
+  //处理侧滑菜单的点击事件
+  @Override
+  public void onClick(View view) {
+    switch ((int) view.getTag()) {
+      case 0:
+        Toast.makeText(context, "Home", Toast.LENGTH_SHORT).show();
+        break;
+      case 1:
+        Toast.makeText(context, "Records", Toast.LENGTH_SHORT).show();
+        break;
+      case 2:
+        Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show();
+        break;
+    }
+    menu.closeMenu();
+  }
+
+  @Override
+  public boolean dispatchTouchEvent(MotionEvent ev) {
+    return menu.dispatchTouchEvent(ev);
+  }
+}
--- app/src/main/java/com/wj/menu/MainActivity.java
+++ app/src/main/java/com/wj/menu/MainActivity.java
@@ -1,13 +1,33 @@
 package com.wj.menu;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+  @InjectView(R.id.main_menu_AndroidResideMenu)
+  Button mainMenuAndroidResideMenu;
+
+  private Context context;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.inject(this);
+    context = this;
+  }
+
+  @OnClick(R.id.main_menu_AndroidResideMenu)
+  public void onViewClicked() {
+    Intent intent = new Intent(context, AndroidResideMenuActivity.class);
+    startActivity(intent);
   }
 }
